54fda753ae5fd3aebf92b6ece43f89bf10d5be9f
Vahid <v.rowghanian@gmail.com>
2021-08-02 01:22:23 +0430

    update

--- im_unity.m
+++
@@ -1,7 +0,0 @@
-function imu = im_unity(im)
-% imu is double type in range 0-1
-im = double(im);
-immin = repmat(min(min(im)), size(im,1), size(im,2));
-immax = repmat(max(max(im)), size(im,1), size(im,2));
-imu = (im - immin)./(immax - immin);
-end%% function
